222f0db0826bb8dd800c3805c6004b0bebf148e5
Aerithe77 <gouesbet.vincent@gmail.com>
2018-12-28 17:29:48 +0100

    modification mineur de formulation des réponses

--- exo_03.rb
+++ exo_03.rb
@@ -1,4 +1,4 @@
 puts "Bonjour, monde !
 # Et avec une voix sexy, ça donne : Bonjour, monde !"
 
-#Le programme ne fonctionne pas car on utilise la concaténation d'un entier dans un string alors qu'on veut concaténer deux strings.
+# Il se passe rien de particulier mis à part l'ajoue du caratère # dans le string puis l'affichage du string.
--- exo_05.rb
+++ exo_05.rb
@@ -10,15 +10,15 @@ puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #On affiche le string "Est-ce q
 
 puts 3 + 2 < 5 - 7 #On additionne 3+2, On soustrait 5-7, On vérifie la condition 5<-2, on affiche la valeur "fasle" résultante de la vérification de la condition
 
-puts "Ça fait combien 3 + 2 ? #{3 + 2}" #On additionne 3+2 et on concatène le résultat "5" dans le string "Ça fait combien 3 + 2 ?" puis on affiche le string résultant.
-puts "Ça fait combien 5 - 7 ? #{5 - 7}" #On additionne 5-7 et on concatène le résultat "-2" dans le string "Ça fait combien 5 - 7 ?" puis on affiche le string résultant.
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #On additionne 3+2 et on concatène le résultat: "5", dans le string "Ça fait combien 3 + 2 ?" puis on affiche le string résultant.
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" #On additionne 5-7 et on concatène le résultat: "-2", dans le string "Ça fait combien 5 - 7 ?" puis on affiche le string résultant.
 
 puts "Ok, c'est faux alors !" #On affiche le string "Ok, c'est faux alors !"
 
 puts "C'est drôle ça, faisons-en plus :" #On affiche le string "C'est drôle ça, faisons-en plus :"
 
-puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #On vérifie la condition 5>-2 puis on concatène le résultat "True" dans le string "Est-ce que 5 est plus grand que -2 ?", et enfin on affiche le string résultant.
-puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #On vérifie la condition 5>=-2 puis on concatène le résultat "True" dans le string "Est-ce que 5 est supérieur ou égal à -2 ?", et enfin on affiche le string résultant.
-puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #On vérifie la condition 5<=-2 puis on concatène le résultat "False" dans le string "Est-ce que 5 est inférieur ou égal à -2 ?", et enfin on affiche le string résultant.
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #On vérifie la condition 5>-2 puis on concatène le résultat: "True", dans le string "Est-ce que 5 est plus grand que -2 ?", et enfin on affiche le string résultant.
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #On vérifie la condition 5>=-2 puis on concatène le résultat: "True", dans le string "Est-ce que 5 est supérieur ou égal à -2 ?", et enfin on affiche le string résultant.
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #On vérifie la condition 5<=-2 puis on concatène le résultat: "False", dans le string "Est-ce que 5 est inférieur ou égal à -2 ?", et enfin on affiche le string résultant.
 
-# #{} défini une concaténation d'un entier dans un string.
+# #{} défini une concaténation d'une valeur dans un string.
