91bf3194a0a4cfa84ce9421790fdaefd266941b8
Aerithe77 <gouesbet.vincent@gmail.com>
2018-12-29 16:53:28 +0100

    Finalisation du préparcours

--- pyramide.rb
+++ pyramide.rb
@@ -1,6 +1,6 @@
 puts "Combien d'étages veux-tu pour ta pyramide ? Donne un nombre entre 1 et 25"
 etage = gets.chomp.to_i
-brique = 0
+brique = 1
 espace = etage - 1
 if etage > 25
   puts "Tu est ambitieux toi ! Malheuresement nous ne pouvont te fournir ta pyramide... Trop d'étages"
@@ -8,16 +8,14 @@ if etage > 25
 elsif (etage < 25) && (etage > 0)
   puts "Voici ta pyramide"
     etage.times do
-        espace.times do
-          print " "
-          end
-            brique.times do
-              puts "#"
-              brique = brique + 1
-              end
-      if brique == 0
-      puts "#"
+      espace.times do
+      print " "
       end
-      espace = espace - 1
+        brique.times do
+        print "#"
+        end
+        espace = espace - 1
+        brique = brique + 1
+        puts " "
     end
-  end
+end
